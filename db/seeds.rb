8b16ccc6f3c6617ace21b1e924383d7a682838d8
Karen Sijbrandij <kxesijbrandij@yahoo.co.uk>
2012-11-21 11:12:46 +0100

    Add resources to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+resources = Resource.create([{title: 'Visualmess', description: 'Guide to creating understandable and attractive designs', url: 'http://www.visualmess.com'}, {title: 'Rubular', description: 'Ruby regular expression editor.', url: 'http://rubular.com'}, {title: 'Railstutorial', description: 'Ruby on Rails tutorial screencasts.', url: 'http://ruby.railstutorial.org'}])
