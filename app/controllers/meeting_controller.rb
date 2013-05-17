be91cc1138cd57c8363f2f2ba47026365528b4d5
Karen Sijbrandij <kxesijbrandij@yahoo.co.uk>
2013-05-17 22:03:19 +0200

    Revert "Add events resource, import for meetup events, functionality for creating events manually. #23"
    This reverts commit 894528490bda00fbae92e13e1e6679dc6d80d014.

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -3,6 +3,9 @@ class MeetingController < ApplicationController
 	GROUPNAME = 'RailsGirls-nl'
 
 	def index
-    @events = Event.where(status: "upcoming")
+
+		#@member = MeetupMember.for_member('47207762')
+		#@events = MeetupEvent.for_group_name(GROUPNAME)
+		#@group = MeetupGroup.for_group_name(GROUPNAME)
 	end
 end
