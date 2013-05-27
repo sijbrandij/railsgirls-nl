dfc5b44c409c9b0cf79cb4ad7721c2a0bc5041c4
Karen Sijbrandij <kxesijbrandij@yahoo.co.uk>
2013-05-27 08:25:27 +0200

    Turn on caching. #23

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -3,10 +3,8 @@ class MeetingController < ApplicationController
 	GROUPNAME = 'RailsGirls-nl'
 
 	def index
-    # @events = Rails.cache.fetch 'meetup_events', expires_in: 10.minute do
-    #   MeetupEvent.for_group_name(GROUPNAME)
-    # end
-    @meetup_events = MeetupEvent.for_group(4812372)
-    #raise @events.count.inspect
+    @meetup_events = Rails.cache.fetch 'meetup_events', expires_in: 10.minute do
+      MeetupEvent.for_group(4812372)
+    end
   end
 end
