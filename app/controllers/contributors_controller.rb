969a541a1d9cf3f6df9325638db19a149df9859a
niila <peatypurple@gmail.com>
2013-07-04 21:11:15 +0200

    Cache github contributors for 10 minutes

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -2,6 +2,8 @@ class ContributorsController < ApplicationController
   GITHUB_CONTRIBUTORS_URI = 'https://api.github.com/repos/sijbrandij/railsgirls-nl/contributors'
 
   def index
-    @contributors = JSON.parse(Faraday.get(GITHUB_CONTRIBUTORS_URI).body)
+    @contributors = Rails.cache.fetch 'contributors', expires_in: 10.minute do
+      JSON.parse(Faraday.get(GITHUB_CONTRIBUTORS_URI).body)
+    end
   end
 end
