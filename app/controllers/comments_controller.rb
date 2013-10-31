756da9f6c7f3c068d6aabd352a8df7c8b5aad6f0
Karen Sijbrandij <karen.sijbrandij@gmail.com>
2013-10-30 20:01:27 -0700

    Configuring Akismet. #39

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,15 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(params[:comment])
-    if @comment.save
-      redirect_to resource_path(@comment.resource), notice: "Comment saved!"
-    else
-      render 'new'
+    if !@comment.spam?
+      if @comment.save
+        redirect_to resource_path(@comment.resource), notice: "Comment saved!"
+      else
+        render 'new'
+      end
     end
   end
+
   def new
   	@comment = Comment.new
   end
