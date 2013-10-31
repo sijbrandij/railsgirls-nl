502a4cf5403739acbfcd92c105a43035ed1690be
Karen Sijbrandij <karen.sijbrandij@gmail.com>
2013-10-30 20:45:01 -0700

    Get rakismet working plus do some updates on layout. #39

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,4 +13,20 @@ class CommentsController < ApplicationController
   def new
   	@comment = Comment.new
   end
+
+  def mark_as_spam
+    @comment = Comment.find(params[:id])
+    if @comment
+      flash[:notice] = "Thanks for your feedback!" if @comment.spam!
+    end
+    redirect_to resource_path(@comment.resource)
+  end
+
+  def unmark_as_spam
+    @comment = Comment.find(params[:id])
+    if @comment
+      flash[:notice] = "Thanks for your feedback!" if @comment.ham!
+    end
+    redirect_to resource_path(@comment.resource)
+  end
 end
