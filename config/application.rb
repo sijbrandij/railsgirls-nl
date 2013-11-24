254c0dc415300d6adcb5d1ba084353513175ae6f
Hester van Wijk <hestervanwijk@live.nl>
2013-11-24 15:07:47 +0100

    removed the remaining rakismet stuff

--- config/application.rb
+++ config/application.rb
@@ -58,9 +58,5 @@ module RailsGirls
 
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
-
-    # Akismet credentials
-    config.rakismet.key = 'b500630f83a3'
-    config.rakismet.url = 'http://railsgirls.nl'
   end
 end
