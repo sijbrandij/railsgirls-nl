5b0458cbdb7179274ea6c4638c02d1582e72af56
Erwin Boskma <erwin@datarift.nl>
2014-03-25 22:22:54 +0100

    Fixed some deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,9 @@ module RailsGirls
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    #
+    # No longer needed due to Strong Parameters
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
