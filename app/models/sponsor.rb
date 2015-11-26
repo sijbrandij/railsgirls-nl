4d52d0437789f7743e8102feadd4b7531c88aa9a
karen <karen.sijbrandij@gmail.com>
2015-11-26 13:53:32 -0800

    Improve styling of home and form for sponsors.

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -2,5 +2,6 @@ class Sponsor < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates_format_of :url, with: URI::regexp(%w(http https))
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png"
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 end
