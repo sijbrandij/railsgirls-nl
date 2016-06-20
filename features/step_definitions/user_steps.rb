1299ced1a3d43764e4a0a101b57516aa5f1077b9
Melanie <makes.melly@gmail.com>
2016-06-20 22:57:01 +0200

    This adds a User factory to the tests

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,3 @@
 Given(/^that I am a logged in user$/) do
-  pending # express the regexp above with the code you wish you had
+  @user = FactoryGirl.create :user, email: Faker::Internet.email, :password => 'letmeinplease', :password_confirmation => 'letmeinplease'
 end
