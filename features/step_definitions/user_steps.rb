52b1cbc160c778d1f094e483dffea8047c8a2185
Melanie <makes.melly@gmail.com>
2016-06-20 23:00:47 +0200

    Adds user logging in and updates deprecated syntax

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,8 @@
 Given(/^that I am a logged in user$/) do
-  @user = FactoryGirl.create :user, email: Faker::Internet.email, :password => 'letmeinplease', :password_confirmation => 'letmeinplease'
+  @email = Faker::Internet.email
+  @user = FactoryGirl.create :user, email: @email, :password => 'letmeinplease', :password_confirmation => 'letmeinplease'
+  visit '/users/sign_in'
+  fill_in 'Email', with: @email
+  fill_in 'Password', with: 'letmeinplease'
+  click_on 'Log in'
 end
