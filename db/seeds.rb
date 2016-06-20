e7a3951b33c6a34c7db06b649ed02270685dd98a
Melanie <makes.melly@gmail.com>
2016-06-20 22:20:19 +0200

    Adds test user to seedsfile for debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-resources = Resource.create([{title: 'Visualmess', description: 'Guide to creating understandable and attractive designs', url: 'http://www.visualmess.com'}, {title: 'Rubular', description: 'Ruby regular expression editor.', url: 'http://rubular.com'}, {title: 'Railstutorial', description: 'Ruby on Rails tutorial screencasts.', url: 'http://ruby.railstutorial.org'}])
+resources = Resource.create(
+    [{
+      title: 'Visualmess',
+      description: 'Guide to creating understandable and attractive designs',
+      url: 'http://www.visualmess.com'
+      },
+    {
+      title: 'Rubular',
+      description: 'Ruby regular expression editor.',
+      url: 'http://rubular.com'
+      },
+    {
+      title: 'Railstutorial',
+      description: 'Ruby on Rails tutorial screencasts.',
+      url: 'http://ruby.railstutorial.org'
+      }])
+
+user = User.create!(
+  :email => 'jane@example.com',
+  :password => 'letmeinplease',
+  :password_confirmation => 'letmeinplease'
+  )
